b292f7318ee133860731dcd944fe36bf22c41c43
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-01-12 00:31:21 +0530

    Update bookingRoutes.js

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Booking from "../models/Booking.js";
 import Event from "../models/Event.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
