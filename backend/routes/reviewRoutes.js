1dc3b72907b2ce9634ad0079593a3a8487583454
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-01-12 00:35:09 +0530

    Update reviewRoutes.js

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,14 +1,13 @@
+// backend/routes/reviewRoutes.js
 import express from "express";
-
-import Review from "../models/Review";
+import Review from "../models/Review.js";
 
 const router = express.Router();
 
 // POST Review
 router.post("/", async (req, res) => {
     try {
-        const review = new Review(req.body);
-        await review.save();
+        const review = await Review.create(req.body);
         res.status(201).json(review);
     } catch (err) {
         res.status(500).json({ error: err.message });
