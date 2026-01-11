2117a6d42c39255c87f904d8f88cc8c0f126f414
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-01-12 00:14:59 +0530

    Update Review.js

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,11 +1,32 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const reviewSchema = new mongoose.Schema({
-    eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true },
-    user: { type: String, default: "Anonymous" },
-    rating: { type: Number, required: true, min: 1, max: 5 },
-    comment: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-});
+const reviewSchema = new mongoose.Schema(
+  {
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true
+    },
+    user: {
+      type: String,
+      default: "Anonymous"
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5
+    },
+    comment: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
-module.exports = mongoose.model("Review", reviewSchema);
+const Review = mongoose.model("Review", reviewSchema);
+
+export default Review;
